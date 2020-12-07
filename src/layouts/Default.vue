db6d8dbf21a540ee867d3a7f875440b73044b5c3
Jônatas Oliveira <contato@jonatasoliveira.me>
2020-12-07 19:49:16 -0300

    inicial commit

---
+++ src/layouts/Default.vue
@@ -0,0 +1,42 @@
+<template>
+  <main class="layout" role="main">
+    <Header />
+    <slot />
+    <div class="footer">
+      <p>
+        Built with
+        <a class="link" href="//gridsome.org">Gridsome</a>
+        & Made with ❤️ by
+        <a class="link" href="//twitter.com/lauragift_">Gift Egwuenu</a>
+      </p>
+    </div>
+  </main>
+</template>
+
+<style>
+body {
+  font-family: -apple-system,system-ui,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,sans-serif;
+  margin:0;
+  padding:0;
+  line-height: 1.5;
+}
+
+.layout {
+  max-width: 760px;
+  margin: 0 auto;
+  padding-left: 20px;
+  padding-right: 20px;
+}
+
+.header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-bottom: 20px;
+  height: 80px;
+}
+
+.nav__link {
+  margin-left: 20px;
+}
+</style>
---
+++ src/pages/Index.vue
@@ -0,0 +1,41 @@
+<template>
+  <Layout>
+    <header class="header">
+      <h1>Jônatas Oliveiar</h1>
+      <p>A simple blog designed with Gridsome</p>
+    </header>
+    <section class="posts">
+        <PostList v-for="edge in $page.allPost.edges" :key="edge.node.id" :post="edge.node" />
+    </section>
+  </Layout>
+</template>
+
+<script>
+import PostList from "@/components/PostList";
+export default {
+  components: {
+    PostList,
+  },
+  metaInfo: {
+    title: "A simple blog",
+  },
+};
+</script>
+
+<page-query>
+query {
+  allPost {
+    totalCount
+    edges {
+      node {
+        id
+        title
+        timeToRead
+        description
+        date (format: "D MMMM YYYY")
+        path
+      }
+    }
+  }
+}
+</page-query>
